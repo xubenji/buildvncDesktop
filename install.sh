07afd9cd60a3f02b260ad2fc031d8ea0d4eb02e1
Benji Xu <459547070@qq.com>
2026-01-24 20:38:58 -0500

    Update install.sh
    不更新源

--- install.sh
+++ install.sh
@@ -1,4 +1,3 @@
-sudo apt upgrade -y
 sudo apt install --no-install-recommends xserver-xorg-core xserver-xorg-video-all xserver-xorg-input-all xinit x11-xserver-utils -y
 sudo apt install --no-install-recommends lxde-core lxsession lxsession-logout lxpanel lxappearance lxpolkit openbox -y
 sudo apt install --no-install-recommends lightdm lightdm-gtk-greeter -y
